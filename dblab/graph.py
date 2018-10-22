d5ebdf5f5cb9a0308f9e68dbd5531062a6406bd3
jeongcm <gstar120@a.ut.ac.kr>
2018-10-22 14:04:07 +0900

    1022

--- dblab/graph.py
+++ dblab/graph.py
@@ -1,15 +1,13 @@
 import pymysql
-import unittest
 import sys
 import socket
-import pickle
 from dijkstar import find_path, NoPathError, Graph
 from dijkstar.algorithm import single_source_shortest_paths
-import test
+import module
 from collections import OrderedDict
 
 class TcpHandler():
-
+ 	
 	#  def handle(self):
 	#      print("client ip : {0}".format(self.client_address[0]))
 	#      sock = self.request
@@ -31,21 +29,16 @@ class TcpHandler():
 
 class passenger():
 
-		def find_default_path(self, pgraph,s,d):
+		def find_default_path(self, pgraph,s,clone_node):
 			con = pymysql.connect(host='localhost', user='root', password='1', db='drive2', charset='utf8')
 
 			curs = con.cursor()
 		
-			#  sql = "SELECT id , MIN(6371*acos(cos(radians({0}))*cos(radians(gps.lat))*cos(radians(gps.lon) - radians({1})) + sin(radians({2}))*sin(radians(gps.lat)))) AS distance , lat, lon FROM jcm_drive.gps where id like 'A3%' group by id order by distance limit 4".format(x,y,x);
-			#  tonode_sql = "select tonode from drive2.a3_link_view where fromnode= "
-			#  curs.execute(tonod_sql+"'" +linkId+"';")
-	
-			#  to_node=curs.fetchall()
 			# '155M10640204', 155M10660204, 155M10680204, 155M10690204,155M10720204,155M10740204, 155M10760204, 155M10790204a
 			node_list = []
 			result_path= []
-			node = ['155M10640204','155M10690204','155M10660204','155M10720204','155M10740204','155M10810204']
-			path=test.Tests()
+			node = ['155M10720204','155M10740204','155M10810204']
+			path=module.Tests()
 			result = path.find_short_path(pgraph,s,node)
 			node_list+=result
 			for path_node in node_list:
@@ -55,10 +48,10 @@ class passenger():
 				#  print(nodes)
 				result_path+=nodes
 				s= path_node
-			destination_result = find_path(pgraph,s,d)
-			nodes,edges,costs,total_cost = destination_result
-			result_path +=nodes
-			OrderedDict.fromkeys(result_path)
+			#  destination_result = find_path(pgraph,s,d)
+			#  nodes,edges,costs,total_cost = destination_result
+			#  result_path +=nodes
+			#  OrderedDict.fromkeys(result_path)
 			return result_path
 
 
@@ -66,8 +59,14 @@ if __name__ == '__main__':
 	if len(sys.argv) < 2:
 		print('{0} <BIND IP>'.format(sys.argv[0]))
 		sys.exit()
-	result_string = ""
-	graph_object=test.Tests()
+	con = pymysql.connect(host='localhost', user='root', password='1', db='jcm_drive', charset='utf8')
+
+	curs = con.cursor()
+		
+	fromnode_sql = "select fromnode from drive2.a3_link ono="
+	tonode_sql = "select tonode from drive2.a3_link_view where fromnode= "
+	etc_info_sql = "select tonode, length from jcm_drive.a3_link_view where fromnode = "
+	graph_object=module.Tests()
 	r_graph = graph_object.make_graph()
 	ip = sys.argv[1]
 	port = 7429
@@ -79,16 +78,51 @@ if __name__ == '__main__':
 		while True:
 				s.listen(1)
 				conn, addr = s.accept()
+				inputnode_list=[]
+				result_string = ""
 				data = conn.recv(1024)
 				if not data :
 					continue
+				print(data)
+				data_list = data.split('/')
+				curs.execute(fromnode_sql+"'" +data_list[0]+"';")
+				fromnode_row= curs.fetchall()
+				fromnode=list(map(lambda x:x, fromnode_row))
+				curs.execute(tonode_sql+"'" +fromnode[0]+"';")
+				startnode_row=curs.fetchall()
+				startnode=list(map(lambda x:x, startnode_row))
+				print('startnode = {0}'.format(startnode[0]))
+				data_list.remove(data_list[0])
+				for i in range(2):
+					linkid_sql = "SELECT ono, MIN(6371*acos(cos(radians({0}))*cos(radians(gps.lat))*cos(radians(gps.lon) - radians({1})) + sin(radians({2}))*sin(radians(gps.lat)))) AS distance FROM jcm_drive.gps where id like 'A3%' group by id order by distance limit 1".format(data_list[i],data_list[i+1],data_list[i]);
+					curs.execute(linkid_sql)
+					link_id_row = curs.fetchall()
+					link_id = list(map(lambda x:x, link_id_row[0]))
+					print('link_id : {0}'.format(link_id[0]))
+					curs.execute(fromnode_sql+"'" +link_id[0]+"';")
+					fromnode_row= curs.fetchall()
+					fromnode=list(map(lambda x:x, fromnode_row))
+					print('fromnode : {0}'.format(fromnode[0]))
+					curs.execute(tonode_sql+"'" +fromnode[0]+"';")
+					input_node_row=curs.fetchall()
+					input_node=list(map(lambda x:x, input_node_row))
+					i+=1
+					inputnode_list+=input_node[0]
+					print('inputnode = {0}'.format(input_node[0]))
+
 				ps = passenger()
-				nodes = ps.find_default_path(r_graph,'155M10630204','155M10820204')
+				nodes = ps.find_default_path(r_graph,startnode[0],inputnode_list)
 				for i in range(len(nodes)):
 						pop_index=nodes.pop(0)
 						result_string+=str(pop_index)
+						curs.execute(etc_info_sql+"'" +pop_index+"';")
+						etc_info_sql_row = curs.fetchall()
+						print("etc_info_row 0 : {0} . etc_info_row1 : {1}".format(etc_info_sql_rowl[0],etc_info_sql_row[1]))
+						result_string+=" "
+						result_string+=str(etc_info_sql_row[0])
+						result_string+=" "
+						result_string+=str(etc_info_sql_row[1])
 						result_string+=" "
 				print(result_string)
-				print(data)
 
 
